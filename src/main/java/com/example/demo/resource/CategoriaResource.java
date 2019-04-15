1a65b45a9550cdb562c9adbbab15b62b7a5f6b36
Felipe Carollo <felipe.carollo@unochapeco.edu.br>
2019-04-15 10:52:49 -0300

    Modelo Categoria
    Add Repository e resource de categoria.

---
+++ src/main/java/com/example/demo/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package com.example.demo.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Respository.CategoriaRepository;
+import com.example.demo.model.Categoria;
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listar(){
+		return categoriaRepository.findAll();	
+	}
+	
+}
