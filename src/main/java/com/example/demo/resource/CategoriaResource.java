94cba86ac6861ad0cb71b1cf6a4d11b47ee7668a
Felipe Carollo <felipe.carollo@unochapeco.edu.br>
2019-04-15 16:51:17 -0300

    Validando valores inválidos com bean validation
    Melhorando o handlerException com mensagens personalizadas. 
    Criando o messages.properties, para conseguir alterar a mensagem de erro padrão de @NotNull e @Size

--- src/main/java/com/example/demo/resource/CategoriaResource.java
+++ src/main/java/com/example/demo/resource/CategoriaResource.java
@@ -5,17 +5,20 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.Respository.CategoriaRepository;
@@ -33,7 +36,7 @@ public class CategoriaResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) { //anotação requestBody é para que o JSON chegue em formato objeto para fazer o save
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) { //anotação requestBody é para que o JSON chegue em formato objeto para fazer o save
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
 		
@@ -43,6 +46,7 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).body(categoriaSalva);	
 	}
 	
+
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Optional<Categoria>> buscarPeloCodigo(@PathVariable Long codigo) {
 		Optional<Categoria> categoria = categoriaRepository.findById(codigo);
