7ef7dd2c824c53e04d1c9494ea5e0e1ab87f4f82
Felipe Carollo <felipe.carollo@unochapeco.edu.br>
2019-04-17 15:23:37 -0300

    Usando eventos para adicionar Header Location

---
+++ src/main/java/com/example/demo/event/listener/RecursoCriadoListener.java
@@ -0,0 +1,35 @@
+package com.example.demo.event.listener;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.demo.event.RecursoCriadoEvent;
+
+
+//Listener é o cara que ouve o evento
+//Ele ta ouvindo para recurso criado event
+// anotado como Component para ser um componente do spring
+
+@Component
+public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoEvent> {
+
+	@Override
+	public void onApplicationEvent(RecursoCriadoEvent recursoCriadoEvent) {
+		HttpServletResponse response = recursoCriadoEvent.getResponse();
+		Long codigo = recursoCriadoEvent.getCodigo();
+		
+		adicionarHeaderLocation(response, codigo);
+	}
+
+	private void adicionarHeaderLocation(HttpServletResponse response, Long codigo) {
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(codigo).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+	} 
+
+}
