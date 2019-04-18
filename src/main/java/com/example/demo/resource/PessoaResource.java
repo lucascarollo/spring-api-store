d13dbc9d1b19de9fec8697c0b596f8022b997e96
Felipe Carollo <felipe.carollo@unochapeco.edu.br>
2019-04-18 11:27:46 -0300

    Atualização de pessoa com put

--- src/main/java/com/example/demo/resource/PessoaResource.java
+++ src/main/java/com/example/demo/resource/PessoaResource.java
@@ -5,14 +5,17 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,11 +41,15 @@ public class PessoaResource {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
+	
+	//@PathVariable = Serve para obter variáveis de uma URL
+	//@RequestBody = Para pega o conteúdo todo de uma URL e formar um objeto
+	
 	@PostMapping
 	public ResponseEntity<Pessoa> criar (@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		
 		Pessoa pessoaSalva = pessoaService.salvar(pessoa);
-		//this --> Quem criou o evento? this de pessoaResource
+		//this --> Quem criou o evento? this de pessoaResource (Explicando os parametros de dentro de recursocriadoevent)
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);	
@@ -64,7 +71,13 @@ public class PessoaResource {
 		pessoaRepository.deleteById(codigo);
 	}
 	
-	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+		
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+		return ResponseEntity.ok(pessoaSalva);
+		
+	}
 	
 	
 	
--- src/main/java/com/example/demo/service/PessoaService.java
+++ src/main/java/com/example/demo/service/PessoaService.java
@@ -2,7 +2,11 @@ package com.example.demo.service;
 
 
 
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.Respository.PessoaRepository;
@@ -12,11 +16,27 @@ import com.example.demo.model.Pessoa;
 public class PessoaService {
 
 	@Autowired
-	private PessoaRepository pessoaRespository;
+	private PessoaRepository pessoaRepository;
 	
 	
 	public Pessoa salvar(Pessoa pessoa) {
-		return pessoaRespository.save(pessoa);
+		return pessoaRepository.save(pessoa);
 	}
 
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		//a BIBLIOTECA SPRING FORNECE UM UTILITARIO PARA COPIAR PROPRIEDADES DE UM OBJETO PARA OUTRO BeanUtils
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		//Copiando de pessoa para pessoaSalva ignorando o código para que o código continue o mesmo
+		return pessoaRepository.save(pessoaSalva);
+	}
+	
+	public Pessoa buscarPessoaPeloCodigo(Long codigo) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+		if(!pessoa.isPresent()) {
+			 throw new EmptyResultDataAccessException(1); // mínimo do tamanho esperado? 1
+			 
+		}
+		return pessoa.get();
+	}
 }
