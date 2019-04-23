cb057924f204ad1d6666be4250a6d9a454b5d0cc
Felipe Carollo <felipe.carollo@unochapeco.edu.br>
2019-04-23 15:40:16 -0300

    Repository Fabrica
    Criação repository Fábrica

---
+++ src/main/java/com/example/demo/Respository/FabricaRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.Respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Fabrica;
+
+public interface FabricaRepository extends JpaRepository<Fabrica, Long> {
+
+}
